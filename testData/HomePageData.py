e1a06ad9b0dea6bdc7f62ea41839a83f74b37346
Quang Vo <qtvo91@yahoo.com>
2021-09-27 15:29:55 -0700

    Added comments in HomePageData module.

--- testData/HomePageData.py
+++ testData/HomePageData.py
@@ -9,7 +9,7 @@ class HomePageData:
         sheet = wb['Sheet1']
         for i in range(1, sheet.max_row+1):
             if sheet.cell(row=i, column=1).value == test_case_name:
-                for j in range(2, sheet.max_column+1):
+                for j in range(2, sheet.max_column+1): # to get columns
                     Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
 
         #print(Dict)
